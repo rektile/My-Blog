7c1aba06ad0fe35cf490b272bb2ab1fbe5f4489a
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-05-21 20:52:03 +0200

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://angeloaguilarmorillo.be',
+	site: 'https://rektile.github.io',
 	integrations: [mdx(), sitemap()],
 });
